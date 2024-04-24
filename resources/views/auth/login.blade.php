88454613a3115375a791eb9676e7ea7edf503621
Rodrigo Nascimento <rnascimento@cbmerj.rj.gov.br>
2024-04-24 09:47:22 -0300

    modificacao view login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -57,11 +57,11 @@
                                     {{ __('Login') }}
                                 </button>
 
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Esqueceu a senha?') }}
-                                    </a>
-                                @endif
+                                <!-- @if (Route::has('password.request')) -->
+                                    <!-- <a class="btn btn-link" href="{{ route('password.request') }}"> -->
+                                        <!-- {{ __('Esqueceu a senha?') }} -->
+                                    <!-- </a> -->
+                                <!-- @endif -->
                             </div>
                         </div>
                     </form>
